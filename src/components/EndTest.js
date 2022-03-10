f142a9b9a367c10377d9aedcc47529cc2a20044c
Yoni Erlich <cool80092@gmail.com>
2022-03-10 23:58:57 +0200

    answerinstance post, probably doesnt work

--- src/components/EndTest.js
+++ src/components/EndTest.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useNavigate, } from "react-router-dom";
+import AnswerInstanceService from "../services/ServicesFolder/AnswerInstanceService";
 
-const EndTest = (params) => {
+
+const EndTest = ({submittedAnswers, testinstanceid}) => {
+  let navigate = useNavigate();
+  useEffect(() => {
+    // const sendUserAnswers = async () => {
+    //   AnswerInstanceService.AddAnswerInstance({submittedAnswers, testinstanceid})
+    // };
+    
+  }, []);
+
   return (
     //send answer ids to api to create answerinstances
     //send info to reportservice
-    
-    <div>{params.testinstanceid}</div>
+
+
+    <div>
+      <p style={{textAlign: 'center'}}> Thank you! results will be mailed to you.</p>
+      <button className="btn" onClick={() => navigate("/")}
+          style={{ backgroundColor: "green",
+          textAlign: "center", display: "flex",
+          justifyContent: "space-between",
+          margin: "50px auto" }}>
+          Go To Home Page
+        </button>
+    </div>
   )
 }
 
